e9716d4f8f3e8c290b06e5d764742e9d2e0a6f74
RECKLESS <66432554+RECKLESS6321@users.noreply.github.com>
2020-08-31 13:47:05 +0530

    Create Maximum Value.py

---
+++ Maximum Value.py
@@ -0,0 +1,6 @@
+def linear_search(search_list):
+  maximum_score_index = None
+  for idx in range(len(search_list)):
+    if not maximum_score_index or search_list[idx] > search_list[maximum_score_index]:
+      maximum_score_index = idx
+  return maximum_score_index
